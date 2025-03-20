0f55f482219cee8ce83b1a0d834802c50391bf42
JoelCamps <jcampsv2324@politecnics.barcelona>
2025-03-20 16:56:23 +0100

    Orm y iniciar sesion

---
+++ NavyBeats C#/Models/Orm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavyBeats_C_.Models
+{
+    public static class Orm
+    {
+        public static dam04Entities bd = new dam04Entities();
+    }
+}
