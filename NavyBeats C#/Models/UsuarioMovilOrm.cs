50f0e318974f9816e96537386707a2fb34a26d59
JoelCamps <jcampsv2324@politecnics.barcelona>
2025-04-01 18:37:57 +0200

    Cambiar orm de Usuarios movil

--- NavyBeats C#/Models/UsuarioMovilOrm.cs
+++ NavyBeats C#/Models/UsuarioMovilOrm.cs
@@ -6,6 +6,31 @@ using System.Threading.Tasks;
 
 namespace NavyBeats_C_.Models
 {
+    public class Musico
+    {
+        public int user_id { get; set; }
+        public string name { get; set; }
+        public string photo { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public int phone_number { get; set; }
+        public string municipality { get; set; }
+    }
+
+    public class Restaurante
+    {
+        public int user_id { get; set; }
+        public string name { get; set; }
+        public string photo { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public int phone_number { get; set; }
+        public string municipality { get; set; }
+        public Nullable<decimal> latitud { get; set; }
+        public Nullable<decimal> longitud { get; set; }
+    }
+
+
     public static class UsuarioMovilOrm
     {
         public static Users SelectUsers()
