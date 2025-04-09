5dc30be468b2685ff2d475083da6ab0c6e9a3c6f
JoelCamps <jcampsv2324@politecnics.barcelona>
2025-04-09 16:28:20 +0200

    correccion de errores

--- NavyBeats C#/Models/CalendarioOrm.cs
+++ NavyBeats C#/Models/CalendarioOrm.cs
@@ -73,7 +73,7 @@ namespace NavyBeats_C_.Models
         /// </summary>
         public static List<DateTime> GetEventDates()
         {
-            using (var context = new NaivyBeatsEntities())
+            using (var context = new dam04Entities())
             {
                 var offerDirDates = context.Offer_dir
                     .Where(o => o.agreement == 1 && o.done == 0 && o.event_date != null)
@@ -102,7 +102,7 @@ namespace NavyBeats_C_.Models
             DateTime fechaSinHora = fecha.Date;
             List<EventoInfo> eventos = new List<EventoInfo>();
 
-            using (var context = new NaivyBeatsEntities())
+            using (var context = new dam04Entities())
             {
                 // Eventos de Offer_dir
                 var eventosDir = (from o in context.Offer_dir
